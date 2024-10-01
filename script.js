7f54945baf33f039e2edf55d05e1cc302d3eca50
César Diego Anovich <iddiegobis@gmail.com>
2024-10-01 15:32:27 -0300

    add keywords

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const palavras = [
     "suicidio","swing","terremoto","terrorismo","terrorista","tiro","tiroteio","tortura","torturada","torturadas","torturado","torturados",
     "torturar","trafico","tragedia","transar","uisque","vibradores","vinho","violencia","vitima","vitimas","vodka","whisky","enforcando","enforcada",
     "enforcado","espanca","espancado","espancada","agredindo","agredido","esfaqueado","esfaqueada","socorro","Voepass","Aéreo","Queda","Caixa-preta",
-    "Anac","IML","Queda","Avião","Acidente","vítima","iml","Vinhedo","Deolane Bezerra","Deolane","presa"
+    "Anac","IML","Queda","Avião","Acidente","vítima","iml","Vinhedo","Deolane Bezerra","Deolane","presa","Irã","Mísseis","Ataque","Líbano"
   ].map(palavra => normalizar(palavra));
   
 function normalizar(texto) {
